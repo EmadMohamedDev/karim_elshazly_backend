9173e3e99a733a3f340a8d602804e49e3b23ca13
Abubakr_Sokarno <abubakr.sokarno@ivas.com.eg>
2018-01-01 09:38:31 +0200

    abubakr, fixing styles in video page with hamza help

--- resources/views/front_end/video_page.blade.php
+++ resources/views/front_end/video_page.blade.php
@@ -4,21 +4,59 @@
     <section class="main-container">
         <h3 class="suggested">الفيديو</h3>
         @if($track)
+
+        <div class="" >
         <?php 
             $init_link = "" ; 
             if($track->content_type==1)
+            {
                 $init_link = url()."/" ; 
+                ?>
+                    <div style="" class="video_parent">
+                        <video id="example_video_1" class="video-js vjs-default-skin vjs-big-play-centered videostyle video_style"
+                            controls preload="auto" width="100%" height="250"
+                            poster="{{$init_link.$track->prev_img}}"
+                            data-setup="{}">
+                            <source src="{{$init_link.$track->path}}" type='video/mp4' />
+                        </video>
+                    </div>
+                <?php 
+            }
+            else{
+                ?> 
+                    <iframe class="videostyle" width="420" height="315"
+                        src="{{$init_link.$track->path}}">
+                    </iframe>
+
+                <?php 
+            }
         ?>
-        <div class="">
-            <video id="example_video_1" class="video-js vjs-default-skin vjs-big-play-centered"
-                controls preload="auto" width="100%" height="250"
-                poster="{{$init_link.$track->prev_img}}"
-                data-setup="{}">
-                <source src="{{$init_link.$track->path}}" type='video/mp4' />
-            </video>
+
         </div>
         @endif
         <br>
+        <style>
+            .video_parent {
+                width:46%;margin:auto;
+            }
+            .videostyle{
+                width: 46%;
+                margin-left: 27%;
+            }
+            @media (max-width:600px){
+                .videostyle{
+                    width:100%;  
+                    margin-left:0 ; 
+                }
+                .video_parent {
+                    width:100%;
+                }
+            }
+            .video_style{
+                margin-left:0;
+                
+            }
+        </style>
 
 
         <!-- suggested-->
