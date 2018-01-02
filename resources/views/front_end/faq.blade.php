ab9be7e37270130ed9a94761bd8d901a0168a1c9
Abubakr_Sokarno <abubakr.sokarno@ivas.com.eg>
2018-01-02 10:15:51 +0200

    abubakr, fixing redirect from faq to home page

--- resources/views/front_end/faq.blade.php
+++ resources/views/front_end/faq.blade.php
@@ -1,10 +1,18 @@
 @extends('front_end.header') 
 @section('content')
+<?php 
+    $query_params = "" ; 
+    if(isset($op_id)&&is_numeric($op_id))
+    {
+        $query_params = "?op_id=$op_id" ; 
+    }
+?> 
+
 <section class="main-container">
     <!-- suggested-->
     <h3 class="suggested">الارشادات</h3>
     {!! $faqs !!}
-    <a href='index.php' class="xs-toggle-btn more">رجوع</a>
+    <a href='{{url("/".$query_params)}}' class="xs-toggle-btn more">رجوع</a>
 </section>
 @stop
 
