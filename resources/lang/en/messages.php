fd077f7ea5f1dcca5e514b915ef4b14554b2cc14
Karim Ahmed <karim.ahmed@ivas.com.eg>
2018-01-17 13:44:19 +0200

    merging

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4,7 +4,11 @@ return [
     'language' => 'عربي',
     'role' => 'Role',
     'create-role' => 'Create Role',
+<<<<<<< HEAD
+    'dashboard' => 'Hamed Zaid',
+=======
     'dashboard' => 'Karim EL-Shazly',
+>>>>>>> karim
     'text-messages' => 'Text Messages',
     'text-messages-send-message' => 'Send Message',
     'text-messages-file-to-sms' => 'File to SMS',
