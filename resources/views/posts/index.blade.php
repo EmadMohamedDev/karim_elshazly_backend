4de323dfbeb5e1e84cbafac7aff0fcf26414edd6
Abubakr_Sokarno <abubakr.sokarno@ivas.com.eg>
2018-01-24 15:09:24 +0200

    abubakr, adding link of the image in posts to the image in front

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -150,17 +150,13 @@
                                     <td>
                                     @foreach($types as $key => $value)
                                        @if($post->content->type_id == $key)
-                                         @if($value == 'Video')
-                                            <input id="url_video" type="text" value="{{url('videos/'.$post->content->id.'?op_id='.$post->operator->id)}}" />
-                                         @elseif($value == 'Audio')
-                                            <input id="url_audio" type="text" value="{{url('audios/'.$post->content->id.'?op_id='.$post->operator->id)}}" />
-                                        @elseif($value == 'Image')
-                                            @if($post->content->content_type == '2')
-                                            <input id="url_photo2" type="text" value="{{$post->content->path}}" />
-                                            @else
-                                            <input id="url_photo1" type="text" value="{{url($post->content->path)}}" />
+                                            @if($value == 'Video')
+                                                <input id="url_video" type="text" value="{{url('videos/'.$post->content->id.'?op_id='.$post->operator->id)}}" />
+                                            @elseif($value == 'Audio')
+                                                <input id="url_audio" type="text" value="{{url('audios/'.$post->content->id.'?op_id='.$post->operator->id)}}" />
+                                            @elseif($value == 'Image') 
+                                                <input id="url_photo1" type="text" value="{{url('photo_page?img='.$post->content->id.'&op_id='.$post->operator->id)}}" />
                                             @endif
-                                         @endif
                                        @endif
                                     @endforeach
                                     </td>
